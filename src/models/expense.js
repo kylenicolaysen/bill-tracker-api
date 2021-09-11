9b94a461687cb5681b5b3fce0dd3d13fc276e957
Kyle Nicolaysen <kylenicolaysen@gmail.com>
2021-09-10 23:20:07 -0700

    added some expense routes/validation

--- src/models/expense.js
+++ src/models/expense.js
@@ -6,6 +6,13 @@ const expenseSchema = new mongoose.Schema({
     required: true,
     trim: true
   },
+  amount: {
+    type: Number,
+    required: false
+  },
+  frequency: {
+    type: String
+  },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
@@ -13,6 +20,16 @@ const expenseSchema = new mongoose.Schema({
   }
 }, {
   timestamps: true
+})
+
+expenseSchema.pre('save', function (next) {
+  console.log('pre-save expense')
+  const frequencyOption = [ 'day', 'week', 'month', 'year' ]
+  const expense = this
+  if(expense.isModified('frequency') && (frequencyOption.find((freq) => expense.frequency === freq) === undefined)) {
+    return next(new Error('invalid FFF'))
+  }
+  next()
 })
 
 const Expense = mongoose.model('Expense', expenseSchema)
