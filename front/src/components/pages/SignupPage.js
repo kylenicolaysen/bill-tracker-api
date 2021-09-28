c885e7434a182c5f178281b7891597bc675f4295
Kyle Nicolaysen <kylenicolaysen@gmail.com>
2021-09-27 21:49:44 -0700

    added signup page and fixed some CSS

--- front/src/components/pages/SignupPage.js
+++ front/src/components/pages/SignupPage.js
@@ -1,30 +1,32 @@
 import React from 'react'
-import { Link, Redirect } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-export default class LoginPage extends React.Component {
+export default class SignupPage extends React.Component {
   state = {
-    loginError: false,
+    signupError: false,
     token: ''
   }
-  handleLoginFormSubmit = async (e) => {
+
+  handleSignupFormSubmit = async (e) => {
     e.preventDefault()
+    const username = e.target.element.username.value.trim()
     const email = e.target.elements.email.value.trim().toLowerCase()
     const password = e.target.elements.password.value.trim()
-    const response = await fetch('/api/user/login', {
+    const response = await fetch('/api/user', {
       method: 'POST',
       // mode: 'cors',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ email, password })
+      body: JSON.stringify({ username, email, password })
     })
     const data = await response.json()
     if (response.status === 400) {
       console.log('failed')
-      this.setState(() => ({ loginError: data.error }))
+      this.setState(() => ({ signupError: data.error }))
     }
     else {
-      this.setState(() => ({token: data.token, loginError: false }))
+      this.setState(() => ({token: data.token, signupError: false }))
       this.props.history.push('/');
       // return <Redirect to="/" />
     }
@@ -35,13 +37,13 @@ export default class LoginPage extends React.Component {
         <div className="auth__page">
           {this.state.loginError && <p>Invalid email or password.</p>}
           <form className="auth__form" onSubmit={ this.handleLoginFormSubmit }>
+            <input placeholder="Name" className="auth__input" type="text" name="username" />
             <input placeholder="Email" className="auth__input" type="text" name="email" />
             <input placeholder="Password" className="auth__input" type="password" name="password" />
-            <button className="primary__submit__button">Login</button>
+            <button className="primary__submit__button">Sign Up</button>
           </form>
-          <Link to="/signup"><button className="secondary__submit__button">Sign Up</button></Link>
-        </div>        
-    
+          <Link to="/login"><button className="secondary__submit__button">Log In</button></Link>
+        </div>            
     )
   }
 }
--- front/src/routers/AppRouter.js
+++ front/src/routers/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import CalendarPage from '../components/pages/CalendarPage'
 import LoginPage from '../components/pages/LoginPage.js'
+import SignupPage from '../components/pages/SignupPage'
 import NotFoundPage from '../components/pages/NotFoundPage'
 import Header from '../components/components/Header'
 
@@ -12,6 +13,7 @@ const AppRouter = () => (
       <Switch>
         <Route path='/' component={CalendarPage} exact />
         <Route path='/login' component={LoginPage} />
+        <Route path='/signup' component={SignupPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
